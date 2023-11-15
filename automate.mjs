b9ea651b4eebb8bd3547ac3f8002602331012635
mdmahfujarrahman <ahmedmahfuj28@gmail.com>
2023-11-15 09:37:44 +0600

    'done'

--- automate.mjs
+++ automate.mjs
@@ -1,33 +1,20 @@
 import { exec } from "child_process";
-
-// exec("git push", (err, stdout, stderr) => {
-//     if (err) {
-//         console.log(`Error: ${err.message}`);
-//         return;
-//     }
-//     if (stderr) {
-//         console.log(stderr, "stderr");
-//         console.log(`stderr: ${stderr}`);
-//         return;
-//     }
-//     console.log(`stdout: ${stdout}`);
-// });
+import { setTimeout } from "timers/promises";
 
 const commands = ["git add .", "git commit -m'done'", "git push"];
 
 for (const command of commands) {
-    exec(command, (err, stdout, stderr) => {
+    exec(command, async (err, stdout, stderr) => {
         if (err) {
-            console.log(`Error: ${err.message}`);
+            console.log(`Error: ${err}`);
             return;
         }
         if (stderr) {
-            console.log(stderr, "stderr");
-            console.log(`stderr: ${stderr}`);
+            console.log(`${command} initialize`);
+
             return;
         }
-        setTimeout(() => {
-            console.log("done");
-        }, 2000);
     });
+    await setTimeout(5000);
+    console.log("wait 5 seconds");
 }
